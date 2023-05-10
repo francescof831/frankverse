9b1b488318f91a8708050790b5677b97c848488c
Francesco Forte <francesco.forte831tech@gmail.com>
2023-05-09 22:34:55 -0400

    initial commit

---
+++ src/main/java/io/frank/frankverse/FrankverseApplication.java
@@ -0,0 +1,13 @@
+package io.frank.frankverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/frank/frankverse/FrankverseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.frank.frankverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
