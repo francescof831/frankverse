9a10c8450837783c9c0664b5a9a81a0ee71eba56
Francesco Forte <francesco.forte831tech@gmail.com>
2023-08-09 22:54:35 -0400

    Access homepage at localhost:8080

--- src/main/java/io/frank/frankverse/controller/HomeController.java
+++ src/main/java/io/frank/frankverse/controller/HomeController.java
@@ -1,13 +1,13 @@
 package io.frank.frankverse.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index"; // Return the name of your Angular's index.html file or template.
+    @RequestMapping({"", "/login", "/products/**"})
+    public String gui() {
+        return "forward:/index.html";
     }
 }
