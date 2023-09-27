f92630d2b7e29aa2aff948b5d281b235f8d0c061
Francesco Forte <francesco.forte831tech@gmail.com>
2023-09-26 22:28:35 -0400

    Upgrade spring boot version, add single page app routing

--- src/main/frontend/src/app/app-routing.module.ts
+++ src/main/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
-{path: 'ui/', component: AppComponent}
+{path: 'ui/', component: AppComponent},
+{path: 'about-me', component: AboutMeComponent}
 ];
 
 @NgModule({
--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
